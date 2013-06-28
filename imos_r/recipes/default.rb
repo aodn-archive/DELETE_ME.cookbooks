438a9f27e606bd73de380bf7576af78820684578
lbesnard <laurent.besnard@utas.edu.au>
2013-06-28 13:41:45 +1000

    Update default.rb

--- imos_r/recipes/default.rb
+++ imos_r/recipes/default.rb
@@ -6,3 +6,24 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+r_package "ncdf4" do
+  action :install
+end
+
+r_package "aqfig" do
+	action :install
+end
+
+r_package "maps" do
+	action :install
+end
+
+r_package "ggplot2" do
+	action :install
+end
+
+r_package "raster" do
+	action :install
+end
+
+
