222da518cc59cebb931379f2e490ab2818cae173
lbesnard <laurent.besnard@utas.edu.au>
2013-06-28 13:51:31 +1000

    Update default.rb

--- imos_user_code_library/recipes/default.rb
+++ imos_user_code_library/recipes/default.rb
@@ -6,3 +6,10 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+ execute "CLONE ucl" do
+    command "git clone https://github.com/aodn/imos_user_code_library'"
+    not_if { ::File.exists?("/home/vagrant/imos_user_code_library")}
+    cwd "/home/vagrant"
+    user "vagrant"
+end
