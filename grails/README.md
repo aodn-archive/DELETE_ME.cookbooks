cf4828fee85f4333387ae677a321739740daa355
Jon Burgess <jkburges@gmail.com>
2013-07-24 20:37:46 +1000

    Added grails cookbook.

---
+++ grails/README.md
@@ -0,0 +1,15 @@
+Description
+===========
+Installs grails 1.3.7 (default) and grails 2.1.0
+
+Requirements
+============
+n/a
+Attributes
+==========
+n/a
+
+Usage
+=====
+`recipe[grails]`
+
---
+++ grails/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "IMOS"
+maintainer_email "developers@emii.org.au"
+license          "All rights reserved"
+description      "Installs/Configures grails"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+recipe "grails::default", "Installs grails"
